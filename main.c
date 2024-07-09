a24d94b466795618c5a519a8fbb8ecf05548091b
jonyski <geleiaeavela@gmail.com>
2024-07-09 01:17:20 -0300

    cleans main to use only the text-to-gif method of gif creation

--- main.c
+++ main.c
@@ -7,6 +7,7 @@ int main(int argc, char const *argv[]) {
 	gif_t orbit = gif_from_file("./gifs/orbit.txt", 250);
 	gif_t fist_bump = gif_from_file("./gifs/fist_bump.txt", 100);
 	gif_t ship_in_the_sea = gif_from_file("./gifs/ship_in_the_sea.txt", 200);
+	
 	render_gif(orbit);
 	// render_gif(fist_bump);
 	// render_gif(ship_in_the_sea);
